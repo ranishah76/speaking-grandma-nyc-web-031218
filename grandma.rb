e0c13dd7ff42d3db05a4debd12d949020fe32273
Rani Shah <rani76@gmail.com>
2018-02-26 17:36:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string !== string.upcase
+  if string == string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     elsif string == "I LOVE YOU GRANGMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
