20cf9fea56a6fac57a90c9827ad859f86e896971
Rani Shah <rani76@gmail.com>
2018-02-26 17:40:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
-def speak_to_grandma(string)
-if string == string.upcase 
-  puts "NO, NOT SINCE 1938!"
-    elsif string == "I LOVE YOU GRANGMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma (phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
